e2f216947dcc3bc1346c30edf1fc2e55235225bb
IJdo <78357961+IJdo@users.noreply.github.com>
2022-05-20 12:30:21 +0200

    First draft full project code
    First draft full project code, by IJdo. Still needs testing if Ultrasonic sensors work.

---
+++ RobotCode/RobotHond/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace RobotHond
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Robot Hond = new Robot();
+
+            while (true)
+            {
+                Hond.Ultra_FRNT.Calculate_distance();
+                Console.WriteLine("afstand = " + Hond.Ultra_FRNT.Distance);
+
+                
+            }
+        }
+    }
+}
